e543811c37d21ce1bf5486239b70507c453a319a
daost2001 <80723897+daost2001@users.noreply.github.com>
2024-02-15 19:19:23 -0800

    new sections added
    -photography section added
    -replaced lorem ipsum
    -added href links
    -added about section
    -added projects
    -added carousel for images
    -styles update

--- JS/main.js
+++ JS/main.js
@@ -51,9 +51,30 @@ ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'})
 
 /*================ typed js =================== */
 const typed = new Typed('.multiple-text',{
-    strings: ["Frontend Developer", 'Web Designer', 'Photographer'],
+    strings: ["Frontend Developer", 'Web Designer', 'Photographer', 'Video Game Enthusiast'],
     typeSpeed:70,
     backSpeed:70,
     backDelay:1000,
     loop:true,
+})
+
+/*================ carousel js =================== */
+const buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach(button => {
+    button.addEventListener("click", () =>{
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        const slides = button
+            .closest("[data-carousel]")
+            .querySelector("[data-slides]")
+        
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex=slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex=0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+    console.log("next")
+    })
 })
